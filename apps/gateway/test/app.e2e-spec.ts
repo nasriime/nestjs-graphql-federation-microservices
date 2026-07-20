55149644b9274961b663bcfbec4af005f2aa4769
nasriime <mnasr4040@gmail.com>
2026-07-20 10:25:22 +0200

    test: mock gateway composition in E2E tests for offline execution

--- apps/gateway/test/app.e2e-spec.ts
+++ apps/gateway/test/app.e2e-spec.ts
@@ -3,8 +3,25 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+jest.mock('@nestjs/graphql', () => {
+  const original = jest.requireActual('@nestjs/graphql');
+  return {
+    ...original,
+    GraphQLModule: {
+      forRoot: () => ({
+        module: class {},
+      }),
+      forRootAsync: () => ({
+        module: class {},
+      }),
+    },
+  };
+});
+
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+
+  jest.setTimeout(30000);
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
